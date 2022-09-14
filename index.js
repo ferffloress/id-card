50b9e14d2f97027fd638522105a3be5614a4ac12
ferffloress <f.flores016@gmail.com>
2022-09-14 11:38:17 -0300

    tp curso react

---
+++ index.js
@@ -0,0 +1,12 @@
+
+document.querySelectorAll(".links")[0].addEventListener("click", function(){
+    document.querySelectorAll(".links")[0].setAttribute('style', 'color: #E4927D');
+});
+
+document.querySelectorAll(".links")[1].addEventListener("click", function(){
+    document.querySelectorAll(".links")[1].setAttribute('style', 'color: #E4927D');
+});
+
+document.querySelectorAll(".links")[2].addEventListener("click", function(){
+    document.querySelectorAll(".links")[2].setAttribute('style', 'color: #E4927D');
+});
